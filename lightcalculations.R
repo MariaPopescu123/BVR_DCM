bbad70a91c508ff1e493779f7e60a83aba0129b9
Maria Popescu <mariapopescu100@gmail.com>
2024-09-30 12:56:31 -0400

    before i change the entire PAR section so that  I coalesce first THEN interpolate

--- lightcalculations.R
+++ lightcalculations.R
@@ -28,10 +28,13 @@ PAR_profiles_filtered <- PAR_profiles |>
             pH = mean(pH, na.rm = TRUE))
 
 
-plot_dat <- PAR_profiles_filtered %>%
+plot_dat <- final_dataCTDmerged %>%
   mutate(Year = year(Date), 
          DayOfYear = yday(Date))|> # Extract year and day of the year
-  select(Date, Year, DayOfYear, PAR_umolm2s, Depth_m)
+  select(Date, Year, DayOfYear, interp_PAR_umolm2s, Depth_m)
+
+redpoints <- plot_dat|>
+  filter(DayOfYear>133, DayOfYear<286)
 
 
 # Plot: x-axis is DayOfYear, y-axis is Year, with a line and highlighted points
@@ -39,7 +42,9 @@ ggplot(plot_dat, aes(x = DayOfYear, y = as.factor(Year), group = Year)) +
   geom_line() +  # Line for each year
   geom_point() +  # Data points
   theme_bw() +
-  labs(x = "Day of Year", y = "Year", title = "PAR profiles data availability") +
+  labs(x = "Day of Year", y = "Year", title = "PAR profiles and CTD merged") +
+  geom_point(data = redpoints, aes(x = DayOfYear, y = as.factor(Year)), 
+             color = "red", size = 3) +  # Highlight max points in red
   scale_x_continuous(breaks = seq(1, 365, by = 30)) +  # Adjust x-axis breaks
   theme(panel.grid.minor = element_blank())  # Optional: remove minor grid lines
 
