a1085c7a1ec8289475b87760dd41da36c1aa48a2
Maria Popescu <mariapopescu100@gmail.com>
2024-11-12 22:23:25 -0500

    fixed metdata

--- DCMBVRdatawrangling.R
+++ DCMBVRdatawrangling.R
@@ -355,30 +355,14 @@ final_datanpratio <- final_datanutrients %>%
   relocate(np_ratio, .before = interp_TN_ugL)
 }
 
-#### Visualizing metdata  ####
-
+####metdata####
 metdata0 <- metdata|>
   mutate(Date = as_date(DateTime))|>
   mutate(DOY = yday(Date))|>
   relocate(DOY, .before = DateTime)|>
   relocate(Date, .before = DateTime)
 
-#### Function for plotting meteorological variables #### 
-metplots <- function(yearz, variable, maxx = NULL){
-  
-  metviz <- metdata0|>
-    filter(year(DateTime) == yearz) #filtering for the year specified
-  
-  
-  ggplot(metviz, aes(x = DateTime, y = {{variable}}))+
-    geom_path()+
-    ggtitle(paste(deparse(substitute(variable)), yearz))+
-    theme_minimal()+
-    scale_y_continuous(limits = c(0, maxx))  # setting consistent y-axis limits
-  
-}
-
-#### Precipitation ####
+##### Precipitation #####
 metdataprecip <- metdata0 |> 
   group_by(Date, year(DateTime))|> 
   mutate(precip_daily = sum(Rain_Total_mm, na.rm = TRUE))|>
@@ -386,51 +370,7 @@ metdataprecip <- metdata0 |>
   relocate(Date, .before = DateTime)|>
   relocate(precip_daily, .before = Rain_Total_mm)
 
-
-
-#b1 <- metplots(2015, precip_daily, maxx = 80)
-#b2 <- metplots(2016, precip_daily, maxx = 80)
-#b3 <- metplots(2017, precip_daily, maxx = 80)
-#b4 <- metplots(2018, precip_daily, maxx = 80)
-#b5 <- metplots(2019, precip_daily, maxx = 80)
-#b6 <- metplots(2020, precip_daily, maxx = 80)
-#b7 <- metplots(2021, precip_daily, maxx = 80)
-#b8 <- metplots(2022, precip_daily, maxx = 80)
-#b9 <- metplots(2023, precip_daily, maxx = 80)
-
-#precips<- plot_grid(
-#  b1, b2, b3,
-#  b4, b5, b6, 
-#  b7, b8, b9,
-#  ncol = 3
-#)
-#print(b1)
-
-#print(precips)
-
-#### Air temps #### 
-
-#b1 <- metplots(2015, AirTemp_C_Average, maxx = 50)
-#b2 <- metplots(2016, AirTemp_C_Average, maxx = 50)
-#b3 <- metplots(2017, AirTemp_C_Average, maxx = 50)
-#b4 <- metplots(2018, AirTemp_C_Average, maxx = 50)
-#b5 <- metplots(2019, AirTemp_C_Average, maxx = 50)
-#b6 <- metplots(2020, AirTemp_C_Average, maxx = 50)
-#b7 <- metplots(2021, AirTemp_C_Average, maxx = 50)
-#b8 <- metplots(2022, AirTemp_C_Average, maxx = 50)
-#b9 <- metplots(2023, AirTemp_C_Average, maxx = 50)
-
-
-#temps<- plot_grid(
-#  b1, b2, b3,
-#  b4, b5, b6, 
-#  b7, b8, b9,
-#  ncol = 3
-#)
-
-#print(temps)
-
-#### dailyaverage and dailymax for temps #### 
+##### dailyaverage and dailymax for temps #####
 metdatatemps <- metdataprecip |> 
   group_by(Date, year(DateTime))|> 
   mutate(daily_airtempavg = mean(AirTemp_C_Average, na.rm = TRUE))|>
@@ -442,8 +382,98 @@ metdatatemps <- metdataprecip |>
   relocate(mindaily_airtemp, .before = AirTemp_C_Average)|>
   select(Date,daily_airtempavg, maxdaily_airtemp, mindaily_airtemp, precip_daily)
 
+####viz met variables ####
+#air_tempavg
+#maxdaily_airtemp
+#mindaily_airtemp
+#precip_daily
 
-#### precip and temp to final_datanpratio #### 
+metplots <- function(yearz, variable, maxx = NULL){
+  
+  metviz <- metdatatemps|>
+    filter(year(Date) == yearz) #filtering for the year specified
+  
+  
+  ggplot(metviz, aes(x = Date, y = {{variable}}))+
+    geom_path()+
+    ggtitle(paste(deparse(substitute(variable)), yearz))+
+    theme_minimal()+
+    scale_y_continuous(limits = c(0, maxx))  # setting consistent y-axis limits
+  
+}
+
+
+
+#####viz Air temps ##### 
+
+b1 <- metplots(2015, daily_airtempavg, maxx = 50)
+b2 <- metplots(2016, daily_airtempavg, maxx = 50)
+b3 <- metplots(2017, daily_airtempavg, maxx = 50)
+b4 <- metplots(2018, daily_airtempavg, maxx = 50)
+b5 <- metplots(2019, daily_airtempavg, maxx = 50)
+b6 <- metplots(2020, daily_airtempavg, maxx = 50)
+b7 <- metplots(2021, daily_airtempavg, maxx = 50)
+b8 <- metplots(2022, daily_airtempavg, maxx = 50)
+b9 <- metplots(2023, daily_airtempavg, maxx = 50)
+
+
+temps<- plot_grid(
+  b1, b2, b3,
+  b4, b5, b6, 
+  b7, b8, b9,
+  ncol = 3
+)
+
+print(temps)
+
+
+#max daily temp
+b1 <- metplots(2015, maxdaily_airtemp, maxx = 50)
+b2 <- metplots(2016, maxdaily_airtemp, maxx = 50)
+b3 <- metplots(2017, maxdaily_airtemp, maxx = 50)
+b4 <- metplots(2018, maxdaily_airtemp, maxx = 50)
+b5 <- metplots(2019, maxdaily_airtemp, maxx = 50)
+b6 <- metplots(2020, maxdaily_airtemp, maxx = 50)
+b7 <- metplots(2021, maxdaily_airtemp, maxx = 50)
+b8 <- metplots(2022, maxdaily_airtemp, maxx = 50)
+b9 <- metplots(2023, maxdaily_airtemp, maxx = 50)
+
+
+temps<- plot_grid(
+  b1, b2, b3,
+  b4, b5, b6, 
+  b7, b8, b9,
+  ncol = 3
+)
+
+print(temps)
+
+
+
+#####viz precip#####
+b1 <- metplots(2015, precip_daily, maxx = 80)
+b2 <- metplots(2016, precip_daily, maxx = 80)
+b3 <- metplots(2017, precip_daily, maxx = 80)
+b4 <- metplots(2018, precip_daily, maxx = 80)
+b5 <- metplots(2019, precip_daily, maxx = 80)
+b6 <- metplots(2020, precip_daily, maxx = 80)
+b7 <- metplots(2021, precip_daily, maxx = 80)
+b8 <- metplots(2022, precip_daily, maxx = 80)
+b9 <- metplots(2023, precip_daily, maxx = 80)
+
+
+precips<- plot_grid(
+  b1, b2, b3,
+  b4, b5, b6, 
+  b7, b8, b9,
+  ncol = 3
+)
+
+print(precips)
+
+
+
+#### merge metdata to final_datanpratio #### 
 
 metdata_join <- metdatatemps |> 
   group_by(Date) |> 
